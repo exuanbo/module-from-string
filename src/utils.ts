115ca4880e89f804f87015be6fcab0d5f89341fa
exuanbo <exuanbo@protonmail.com>
2021-02-22 10:47:18 +0000

    refactor(types): function `generateOptions`

--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,8 @@ import { ImportOptions } from './import'
 export function generateOptions(options: string | ImportOptions): ImportOptions
 export function generateOptions(options: string | Options): Options
 
-export function generateOptions(options: string | Options): Options {
+export function generateOptions(
+  options: string | Options | ImportOptions
+): Options | ImportOptions {
   return typeof options === 'string' ? { code: options } : options
 }
