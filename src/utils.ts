7b5cfcc2cd5b19bf2b63c4800d46dc326474a5aa
exuanbo <exuanbo@protonmail.com>
2021-08-16 14:33:20 +0100

    refactor: rename variables in `getCallerDirname`

--- src/utils.ts
+++ src/utils.ts
@@ -11,10 +11,10 @@ export const isInESModuleScope = (): boolean => {
 
 export const getCallerDirname = (): string | null => {
   const _prepareStackTrace = Error.prepareStackTrace
-  Error.prepareStackTrace = (_err, stack) => stack
-  const stack = (new Error().stack as unknown as NodeJS.CallSite[]).slice(2)
+  Error.prepareStackTrace = (_err, stackTraces) => stackTraces
+  const callSites = (new Error().stack as unknown as NodeJS.CallSite[]).slice(2)
   Error.prepareStackTrace = _prepareStackTrace
-  const fileName = stack[0].getFileName()
+  const fileName = callSites[0].getFileName()
   return fileName !== null
     ? path.dirname(fileName.startsWith('file://') ? fileURLToPath(new URL(fileName)) : fileName)
     : null
