f179e39da0017e830fde6eced0c1b24ce398ea0a
exuanbo <exuanbo@protonmail.com>
2021-08-19 21:43:54 +0100

    build: warn during transform

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import path from 'path'
-import { transform } from 'esbuild'
+import { formatMessages, transform } from 'esbuild'
 import pkg from './package.json'
 
 export default {
@@ -22,13 +22,25 @@ export default {
         return source.startsWith('.') ? path.resolve(path.dirname(importer), `${source}.ts`) : null
       },
       async transform(code, id) {
-        const { code: transformedCode, map } = await transform(code, {
+        const {
+          code: transformedCode,
+          map,
+          warnings
+        } = await transform(code, {
           format: 'esm',
           loader: 'ts',
           sourcefile: id,
           sourcemap: true,
           target: 'es2019'
         })
+        if (warnings.length > 0) {
+          ;(
+            await formatMessages(warnings, {
+              kind: 'warning',
+              color: true
+            })
+          ).forEach(message => this.warn(message))
+        }
         return {
           code: transformedCode,
           map
