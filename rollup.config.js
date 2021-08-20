09e3b7671326d697bd313efa0afc304924e130d9
exuanbo <exuanbo@protonmail.com>
2021-08-20 14:17:58 +0100

    chore: add back `rollup-plugin-dts`

--- rollup.config.js
+++ rollup.config.js
@@ -1,57 +1,70 @@
 import path from 'path'
 import { formatMessages, transform } from 'esbuild'
+import dts from 'rollup-plugin-dts'
 import pkg from './package.json'
 
-export default {
-  external: ['module', 'path', 'url', 'vm', ...Object.keys(pkg.dependencies)],
-  input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs'
-    },
-    {
-      file: pkg.module,
-      format: 'es'
-    }
-  ],
-  plugins: [
-    {
-      name: 'esbuild',
-      resolveId(source, importer) {
-        return source.startsWith('.') ? path.resolve(path.dirname(importer), `${source}.ts`) : null
+export default [
+  {
+    external: ['module', 'path', 'url', 'vm', ...Object.keys(pkg.dependencies)],
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs'
       },
-      async transform(code, id) {
-        const {
-          code: transformedCode,
-          map,
-          warnings
-        } = await transform(code, {
-          format: 'esm',
-          loader: 'ts',
-          sourcefile: id,
-          sourcemap: true,
-          target: 'es2019'
-        })
-        if (warnings.length > 0) {
-          ;(
-            await formatMessages(warnings, {
-              kind: 'warning',
-              color: true
-            })
-          ).forEach(message => this.warn(message))
-        }
-        return {
-          code: transformedCode,
-          map
-        }
-      },
-      renderDynamicImport() {
-        return {
-          left: 'import(',
-          right: ')'
+      {
+        file: pkg.module,
+        format: 'es'
+      }
+    ],
+    plugins: [
+      {
+        name: 'esbuild',
+        resolveId(source, importer) {
+          return source.startsWith('.')
+            ? path.resolve(path.dirname(importer), `${source}.ts`)
+            : null
+        },
+        async transform(code, id) {
+          const {
+            code: transformedCode,
+            map,
+            warnings
+          } = await transform(code, {
+            format: 'esm',
+            loader: 'ts',
+            sourcefile: id,
+            sourcemap: true,
+            target: 'es2019'
+          })
+          if (warnings.length > 0) {
+            ;(
+              await formatMessages(warnings, {
+                kind: 'warning',
+                color: true
+              })
+            ).forEach(message => this.warn(message))
+          }
+          return {
+            code: transformedCode,
+            map
+          }
+        },
+        renderDynamicImport() {
+          return {
+            left: 'import(',
+            right: ')'
+          }
         }
       }
-    }
-  ]
-}
+    ]
+  },
+  {
+    input: '.cache/index.d.ts',
+    output: {
+      file: pkg.types,
+      format: 'es'
+    },
+    plugins: [dts()]
+  }
+]
