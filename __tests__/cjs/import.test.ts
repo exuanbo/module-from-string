542bd7c984803dd323a6a8f2e44d75e69d7e206a
exuanbo <exuanbo@protonmail.com>
2022-05-17 15:56:37 +0100

    test(cjs): extract function `testImport`

--- __tests__/cjs/import.test.ts
+++ __tests__/cjs/import.test.ts
@@ -1,26 +1,28 @@
 import path from 'path'
 import { importFromString, importFromStringSync } from '../../src/index'
 
-describe('importFromString', () => {
+let importFromStringFn: typeof importFromString | typeof importFromStringSync
+
+const testImport = (): void => {
   it('should work with named export', async () => {
-    const res = await importFromString("export const greet = 'hi'")
+    const res = await importFromStringFn("export const greet = 'hi'")
     expect(res.greet).toBe('hi')
   })
 
   it('should work with default export', async () => {
-    const res = await importFromString("export default 'hi'")
+    const res = await importFromStringFn("export default 'hi'")
     expect(res.default).toBe('hi')
   })
 
   it('should work with relative path import', async () => {
     const modulePath = './fixtures/namedExport.js'
-    const res = await importFromString(`export { greet } from '${modulePath}'`)
+    const res = await importFromStringFn(`export { greet } from '${modulePath}'`)
     expect(res.greet).toBe('hi')
   })
 
   it('should resolve correctly if option `dirname` is provided', async () => {
     const modulePath = './cjs/fixtures/defaultExport.js'
-    const res = await importFromString(`import greet from '${modulePath}'; export default greet`, {
+    const res = await importFromStringFn(`import greet from '${modulePath}';export default greet`, {
       dirname: path.dirname(__dirname)
     })
     expect(res.default).toBe('hi')
@@ -28,7 +30,7 @@ describe('importFromString', () => {
 
   it('should work with absolute path import', async () => {
     const modulePath = path.join(__dirname, 'fixtures/namedExport.js')
-    const res = await importFromString(`export { greet } from '${modulePath}'`)
+    const res = await importFromStringFn(`export { greet } from '${modulePath}'`)
     expect(res.greet).toBe('hi')
   })
 
@@ -37,7 +39,7 @@ describe('importFromString', () => {
 const { code } = transformSync('enum Greet { Hi }', { loader: 'ts' })
 export default code
 `
-    const res = await importFromString(code)
+    const res = await importFromStringFn(code)
     expect(res.default).toMatchInlineSnapshot(`
       "var Greet = /* @__PURE__ */ ((Greet2) => {
         Greet2[Greet2[\\"Hi\\"] = 0] = \\"Hi\\";
@@ -48,63 +50,19 @@ export default code
   })
 
   it('should work if transformOption is provided', async () => {
-    const res = await importFromString("export const greet: () => string = () => 'hi'", {
+    const res = await importFromStringFn("export const greet: () => string = () => 'hi'", {
       transformOptions: { loader: 'ts' }
     })
     expect(res.greet()).toBe('hi')
   })
+}
+
+describe('importFromString', () => {
+  importFromStringFn = importFromString
+  testImport()
 })
 
 describe('importFromStringSync', () => {
-  it('should work with named export', () => {
-    const res = importFromStringSync("export const greet = 'hi'")
-    expect(res.greet).toBe('hi')
-  })
-
-  it('should work with default export', () => {
-    const res = importFromStringSync("export default 'hi'")
-    expect(res.default).toBe('hi')
-  })
-
-  it('should work with relative path import', () => {
-    const modulePath = './fixtures/namedExport.js'
-    const res = importFromStringSync(`export { greet } from '${modulePath}'`)
-    expect(res.greet).toBe('hi')
-  })
-
-  it('should resolve correctly if option `dirname` is provided', () => {
-    const modulePath = './cjs/fixtures/defaultExport.js'
-    const res = importFromStringSync(`import greet from '${modulePath}'; export default greet`, {
-      dirname: path.dirname(__dirname)
-    })
-    expect(res.default).toBe('hi')
-  })
-
-  it('should work with absolute path import', () => {
-    const modulePath = path.join(__dirname, 'fixtures/namedExport.js')
-    const res = importFromStringSync(`export { greet } from '${modulePath}'`)
-    expect(res.greet).toBe('hi')
-  })
-
-  it('should work with import external module', () => {
-    const code = `import { transformSync } from 'esbuild'
-const { code } = transformSync('enum Greet { Hi }', { loader: 'ts' })
-export default code
-`
-    const res = importFromStringSync(code)
-    expect(res.default).toMatchInlineSnapshot(`
-      "var Greet = /* @__PURE__ */ ((Greet2) => {
-        Greet2[Greet2[\\"Hi\\"] = 0] = \\"Hi\\";
-        return Greet2;
-      })(Greet || {});
-      "
-    `)
-  })
-
-  it('should work if transformOption is provided', () => {
-    const res = importFromStringSync("export const greet: () => string = () => 'hi'", {
-      transformOptions: { loader: 'ts' }
-    })
-    expect(res.greet()).toBe('hi')
-  })
+  importFromStringFn = importFromStringSync
+  testImport()
 })
